2f8428c15fa1f1ea412e99bd52ffd50b525883fb
Aaron Berns <anberns@gmail.com>
2019-01-08 15:39:35 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,6 +8,3 @@ def collect(array)
   end
   collection
 end
-
-You are writing a method that behaves just like the real `#collect` method. It should take in an argument of a collection, iterate over that collection using a `while` loop, and execute the code in the block you call it with for each element in the collection (use the `yield` keyword). It should return the modified collection. 
-
