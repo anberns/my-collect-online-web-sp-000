1178de13b0959e7affec330871a8fc6da3e36c5d
Aaron Berns <anberns@gmail.com>
2019-01-08 15:39:32 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+# collect
+def collect(array)
+  i = 0 
+  collection = []
+  while i < array.size 
+    collection << yield(array[i])
+    i += 1 
+  end
+  collection
+end
 
+You are writing a method that behaves just like the real `#collect` method. It should take in an argument of a collection, iterate over that collection using a `while` loop, and execute the code in the block you call it with for each element in the collection (use the `yield` keyword). It should return the modified collection. 
 
